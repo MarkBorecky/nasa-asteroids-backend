7a9e29448eb3f91840fa4d77dcc8bb4181989189
maro <marek.borecki88@gmail.com>
2022-07-04 17:33:48 +0300

    add CORS settings

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from datetime import date
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI
 from models import DateRange
 import asteroid_servise
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/v1/asteroids")
